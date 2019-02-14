e21579eb1029ccdf5ed826cdd5565ce26178f68d
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-02-14 11:02:40 +0000

    [spotify_status.sh]: Show Spotify whenever the program is alive

--- spotify_status.sh
+++ spotify_status.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ "$(playerctl status)" = "Playing" ]; then
-    title=$(exec playerctl metadata xesam:title)
-    artist=$(exec playerctl metadata xesam:artist)
-  echo "$title - $artist"
-else
-  echo ""
-fi
+title=$(exec playerctl metadata xesam:title)
+artist=$(exec playerctl metadata xesam:artist)
+echo "$title - $artist"
